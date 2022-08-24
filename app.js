dd343b9d88585392a3ed6c037cbce33cb3912aca
Bishal Soanr <43300737+Bishsy@users.noreply.github.com>
2022-08-25 00:54:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// const fs = require('fs')
+// //fs.writeFileSync('notes.txt','My name is Bishal sonar ')
+
+// //fs.appendFileSync('notes.txt','I am from Asansol')
+
+require('./ults.js')
+const name = 'Bishal'
+
+console.log(name)
+
+
